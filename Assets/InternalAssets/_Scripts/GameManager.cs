1e74b3fdcd1f1e699d0f059324656972221cc24c
Evgeniy <evgeniy-vysockiy@bk.ru>
2022-03-11 00:00:27 +0300

    Add game manager

--- Assets/InternalAssets/_Scripts/GameManager.cs
+++ Assets/InternalAssets/_Scripts/GameManager.cs
@@ -2,17 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GameState { INTRO, MENU,PLAY }
+public delegate void OnStateChangeHandler();
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    protected GameManager(){}
+    private static GameManager _instance = null;
+    public event OnStateChangeHandler OnStateChange;
+    public GameState gameState { get; private set; }
+
+    public static GameManager Instance
     {
-        
+        get
+        {
+            if (GameManager._instance == null)
+            {
+                DontDestroyOnLoad(GameManager._instance);
+                GameManager._instance = new GameManager();
+            }
+
+            return GameManager._instance;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetGameState(GameState state)
     {
-        
+        gameState = state;
+        OnStateChange?.Invoke();
+    }
+    
+    public void OnApplicationQuit(){
+        GameManager._instance = null;
     }
 }
