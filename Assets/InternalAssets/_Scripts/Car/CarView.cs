187411b5a7ff4530e796474de7cccc17e5fd683d
Evgeniy <evgeniy-vysockiy@bk.ru>
2022-04-12 20:43:40 +0300

    Trigger car

--- Assets/InternalAssets/_Scripts/Car/CarView.cs
+++ Assets/InternalAssets/_Scripts/Car/CarView.cs
@@ -7,16 +7,22 @@ namespace InternalAssets._Scripts.Car
     public class CarView:BaseCarView
     {
         private List<Material> _materials;
+        private Rigidbody _rigidbody;
         private MeshRenderer _meshRenderer;
         private void Awake()
         {
             Initialized();
         }
-
+        
         private void Initialized()
         {
             this._transform = transform;
             _meshRenderer = _transform.gameObject.GetComponent<MeshRenderer>();
+        }
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            Debug.Log("OnTriggerEnter");
         }
 
         public void SetMaterials(Material[] materials)
---
+++ Assets/InternalAssets/_Scripts/DrivePoint/DrivePoint.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrivePoint : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTriggerEnter");
+    }
+}
